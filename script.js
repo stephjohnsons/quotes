6205623f04ef0630734799468bda2cd8b8ace711
Stephen Johnson-Tseu <71110517+stephjohnsons@users.noreply.github.com>
2024-11-26 10:42:44 +0800

    updated API

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ const button = document.querySelector("button");
 const language = document.getElementById("language");
 const reset = document.getElementById("reset");
 
-/* Quotes */ // Powered by Quotable  
-// https://github.com/lukePeavey/quotable
-
-const quoteURL = "https://api.quotable.io/random";
-async function updateQuote() {
-  const result = await fetch(quoteURL);
-  const data = await result.json();
-  quote.innerHTML = data.content;
-  author.innerHTML = ` — ${data.author}`;
-  console.log(data);
-
-  language.classList.remove('invisible');
-  language.classList.add('visible');
+const url = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1&cat=famous';
+const options = {
+	method: 'POST',
+	headers: {
+		'x-rapidapi-key': '8c0274f42amsh44d91c860b8291cp1cc65ajsnaef3874e862d',
+		'x-rapidapi-host': 'andruxnet-random-famous-quotes.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	},
+	body: {}
 };
+
+async function updateQuote() {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+    quote.innerText = data[0].quote || "No quote found.";
+    author.innerText = data[0].author ? `— ${data[0].author}` : "— Unknown";
+    console.log(data);
+
+    language.classList.remove('invisible');
+    language.classList.add('visible');
+  } catch (error) {
+    console.error("Error fetching or displaying the quote:", error);
+  }
+}
 
 button.addEventListener("click", updateQuote);
 
